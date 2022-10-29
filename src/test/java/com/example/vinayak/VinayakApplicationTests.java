2a6ea18729dd781ddee446a1f6ee500b29f5bbf1
Gaurav <kaverigaurav14@gmail.com>
2022-10-29 17:59:48 +0530

    msg

---
+++ src/test/java/com/example/vinayak/VinayakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinayak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinayakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
